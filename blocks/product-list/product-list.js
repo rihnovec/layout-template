35960afb3ff5720afbd51aff15687a28e1d44ef7
Anton Rikhnovets <rihnovec93@mail.ru>
2024-08-29 12:36:21 +0900

    Снижает чувствительность слайдера к коротким свайпам

--- blocks/product-list/product-list.js
+++ blocks/product-list/product-list.js
@@ -8,6 +8,7 @@ import 'swiper/swiper-bundle.css';
 export default() => {
     new Swiper('.product-list-item-slider', {
         slidesPerGroup: 1,
+        threshold: 30,
         pagination: {
             el: '.product-list-item-slider-pagination',
             type: 'bullets',
