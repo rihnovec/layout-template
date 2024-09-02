28c993da949246135c43cfb7f3eb0e29c4c2c98e
ndunbavo <n.dunbavo@z-labs.ru>
2024-09-02 14:55:36 +0900

    Вносит правки

--- blocks/product-list/product-list.js
+++ blocks/product-list/product-list.js
@@ -2,11 +2,30 @@ export default() => {
     new Swiper('.product-list-item-slider', {
         slidesPerGroup: 1,
         threshold: 30,
+        autoplay: {
+            delay: 5000,
+        },
+        effect: isDesktop() ? 'fade' : 'slide',
+        allowTouchMove: !isDesktop(),
         pagination: {
             el: '.product-list-item-slider-pagination',
             type: 'bullets',
             clickable: true,
         },
-    });
+        on: {
+            init(swiper) {
+                const navItems = swiper.el.querySelectorAll('.product-list-item-slider-nav-item')
 
+                navItems.forEach((navItem, index) => {
+                    navItem.addEventListener('mouseenter', () => {
+                        swiper.slideTo(index)
+                    })
+                })
+            }
+        }
+    });
+}
+
+function isDesktop() {
+    return window.innerWidth >= 1280
 }
--- main.js
+++ main.js
@@ -4,19 +4,17 @@ import '@fancyapps/fancybox'
 import '@fancyapps/fancybox/dist/jquery.fancybox.min.css'
 
 import Swiper from 'swiper';
-import {Pagination, Scrollbar, EffectFade} from 'swiper'
+import {Pagination, Scrollbar, EffectFade, Autoplay, Navigation} from 'swiper'
 
-Swiper.use([Pagination, Scrollbar, EffectFade])
+Swiper.use([Pagination, Scrollbar, EffectFade, Autoplay, Navigation])
 window.Swiper = Swiper
 
 import 'swiper/swiper-bundle.css';
 
 import showroomGallery from './blocks/showroom-gallery/showroom-gallery'
 import productList from './blocks/product-list/product-list'
-import cardList from './blocks/card-list/card-list'
 
 document.addEventListener('DOMContentLoaded', () => {
     showroomGallery()
-    cardList()
     productList()
 })
