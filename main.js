2cc1ce59898e4e9b5d1d5a9cc60e5e19a5a47676
Anton Rikhnovets <rihnovec93@mail.ru>
2024-09-03 17:11:15 +0900

    Доработки в билд

--- main.js
+++ main.js
@@ -2,8 +2,14 @@ import './blocks/bundles/showroom.sass'
 
 import showroomGallery from './blocks/showroom-gallery/showroom-gallery'
 import productList from './blocks/product-list/product-list'
+import cardList from './blocks/card-list/card-list.js'
+import {isDesktop} from './blocks/helpers/isDesktop.js'
 
 document.addEventListener('DOMContentLoaded', () => {
     showroomGallery()
     productList()
+
+    if (!isDesktop()) {
+        cardList()
+    }
 })
